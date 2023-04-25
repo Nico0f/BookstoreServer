45aba4465132008abc659187e75d05b39302ac19
nico0f <franconicoletti.14@gmail.com>
2023-04-25 14:12:07 -0300

    firebase

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,16 @@ config();
 
 import admin from 'firebase-admin'
 
-// var serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
-
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   // app.enableCors({credentials: true, origin: 'http://localhost:3000'});
   app.use(cookieParser());
   const configService = app.get(ConfigService);
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(JSON.parse(process.env.FIREBASE)),
-  //   databaseURL: "https://bookstore-3d941-default-rtdb.firebaseio.com"
-  // });
-  console.log('emepezo')
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE)),
+    databaseURL: "https://bookstore-3d941-default-rtdb.firebaseio.com"
+  });
   const port = configService.get<number>('PORT', 3001);
   // app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
